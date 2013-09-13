8ec8f1c73ddbad14e4dcd4a76964e5be6d63ae4a
Xiangxin <elainsxx@gmail.com>
2013-09-14 02:50:55 +0800

    removed some useless scaffolded code

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ MovieReview::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: 'movies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
