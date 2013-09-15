89284f4a6a80c270a09eab42e724795d78b63f26
Ziwei Peng <pzw2041@gmail.com>
2013-09-15 14:59:41 +0800

    merged

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,36 +4,36 @@ class MoviesController < ApplicationController
  	require 'uri'
  	require 'sinatra'
  	require 'net/http'
- 	require 'net/https'	
- 	
- 	# global variables
- 	@@client_id = 'dfab6ac8e7336923fe75d4e7aa48cd0d'
- 	@@client_secret = 'fd1f5a26c5d53e17f8ed38e1aa282a51'
- 	@@auth_url = 'https://cs3213.herokuapp.com'
- 	@@auth_path = '/oauth/token.json'
+ 	require 'net/https'
 
- 	skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token
 
-  	def index
+  # global variables
+  @@client_id = 'dfab6ac8e7336923fe75d4e7aa48cd0d'
+  @@client_secret = 'fd1f5a26c5d53e17f8ed38e1aa282a51'
+  @@auth_url = 'https://cs3213.herokuapp.com'
+  @@auth_path = '/oauth/token.json'
 
-  	end
+  def index
 
-  	def sign_out
-  		session['token'] = nil
-  		redirect_to :action => 'index'
-  	end
+  end
 
-  	# redirect to movies@3213 to get authentication and code
-  	def sign_in
-  		redirect_url = 'http://0.0.0.0:3000/movies/access_token'
-    	url = "https://cs3213.herokuapp.com/oauth/new?client_id=" + @@client_id + "&client_secret=" + @@client_secret + "&redirect_uri=" + redirect_url
-    	redirect_to url
-  	end
+  def sign_out
+  	session['token'] = nil
+  	redirect_to :action => 'index'
+  end
 
-def access_token
+  # redirect to movies@3213 to get authentication and code
+	def sign_in
+ 		redirect_url = 'http://0.0.0.0:3000/movies/access_token'
+   	url = 'https://cs3213.herokuapp.com/oauth/new?client_id=' + @@client_id + '&client_secret=' + @@client_secret + '&redirect_uri=' + redirect_url
+   	redirect_to url
+ 	end
+
+  def access_token
 
     # if correct request
-    if (params[:response_type] == 'code')
+    if params[:response_type] == 'code'
 
      	# code
      	@code = params[:code]	
@@ -42,11 +42,11 @@ def access_token
      	uri = URI.parse(@@auth_url)
      	http = Net::HTTP.new(uri.host, uri.port)
      	http.use_ssl = true
-     	data = "client_id=" + @@client_id + "&client_secret=" + @@client_secret + "&code=" + params[:code]
+     	data = 'client_id=' + @@client_id + '&client_secret=' + @@client_secret + '&code=' + params[:code]
      	headers = {
      	    'Content-Type' => 'application/x-www-form-urlencoded'
      	}
-     	path = @@auth_path	
+     	path = @@auth_path
      	# response
      	resp = http.post(path, data, headers)	
      	@msg = resp.message	
